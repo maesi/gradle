8d2d182ec49be26e6bea0f2a7783dbaca76c16c8
Marcel Arnold <mail@maesi.org>
2016-08-18 21:09:57 +0200

    add Example Class

---
+++ src/main/java/org/maesi/gradle/Example.java
@@ -0,0 +1,8 @@
+package org.maesi.gradle;
+
+public class Example {
+
+    public String getName() {
+        return "Example";
+    }
+}
---
+++ src/test/java/org/maesi/gradle/ExampleTest.java
@@ -0,0 +1,14 @@
+package org.maesi.gradle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void getName() {
+        assertEquals("Example", new Example().getName());
+    }
+
+}
